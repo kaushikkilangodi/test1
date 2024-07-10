d77e055dbfacafe1c16edc8d58809eca74c259c8
kaushikkilangodi <kaushikk883@gmail.com>
2024-07-10 16:06:17 +0530

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+
 
 
 export default defineConfig({
   plugins: [
     react(),
-    TanStackRouterVite(),
+ 
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: [
